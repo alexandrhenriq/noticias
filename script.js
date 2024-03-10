3d880e78b99826356d7119f7bc9f86ec171367ad
Alexandre Henrique Gomes Lopes de Almeida <lopes.alexandre@eaportal.org>
2024-03-10 01:06:12 -0300

    criação da navegação desktop, remoção da classe header mobile e início da responsividade

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener('click', abrirMenu)
 botaoFechar.addEventListener('click', fecharMenu)
